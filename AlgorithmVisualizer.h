57a6d747b1b20414559fca0dd2060ec2fe9741d8
Jusypablo13 <A01638911@tec.mx>
2024-10-06 18:34:55 -0600

    RenderWindow Missing

--- AlgorithmVisualizer.h
+++ AlgorithmVisualizer.h
@@ -18,7 +18,7 @@ class AlgorithmVisualizer {
         Statistics stats;
     
     public:
-        void promptForInput();
+        void promptForInput(sf::RenderWindow &window);
         void displayInputData(sf::RenderWindow &window, const vector<int> &data);
         void executeAlgorithm(const string &algorithmName);
         void showResults(sf::RenderWindow &window, const vector<int> &data, const string &algorithmName);
